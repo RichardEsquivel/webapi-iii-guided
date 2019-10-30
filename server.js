fa14186b6d6d1a70c53780bfee50104430295c2a
RichardEsquivel <rlesquivel@gmail.com>
2019-10-30 12:34:13 -0500

    Stilling getting error advises next() is not defined

--- server.js
+++ server.js
@@ -1,21 +1,39 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
 const hubsRouter = require('./hubs/hubs-router.js');
-
+const morgan = require('morgan');//is a logger 
 
 const server = express();
-
-function dateLoggerAndMethod(req, res, next) {//three amigos
-	console.log(new Date().toISOString());
-	console.log(`${req.baseURL}`)
-	console.log(`${req.method}`);
-	next();
-}
-//global middleware
 server.use(helmet());//third party
 server.use(express.json());//built-in
-server.use(dateLoggerAndMethod);
+function dateLogger(req, res, next) {//three amigos
+	console.log(new Date().toISOString());
 
+	next();
+}
+
+
+
+function gateKeeper(req, res, next) {
+	//new way of reading data sent by the client, data can come in the body  in the url params, and the query string, and the headers
+	const password = req.headers.password || '';
+	if (!password) {
+		res.status(400).json({ you: `I need a password!!!` })
+	}
+
+	else if (password.toLowerCase() === 'mellon') {
+		next();
+	} else {
+		res.status(401).json({ you: 'cannot pass!!' });
+	}
+
+}
+
+//global middleware
+
+server.use(dateLogger());
+
+server.use(morgan('dev'))
 //middlware only used in certain cases, restricting scope
 server.use('/api/hubs', hubsRouter);
 
@@ -29,3 +47,6 @@ server.get('/', (req, res) => {//req, res the homies
 });
 
 module.exports = server;
+
+
+//no passwords in url temporary tokens in URL or in header
