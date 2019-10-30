5f9e5ca2c08c76c8b008ae91570dbf51ea8c548f
RichardEsquivel <rlesquivel@gmail.com>
2019-10-30 12:05:57 -0500

    Commiting

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express'); // importing a CommonJS module
-
+const helmet = require('helmet');
 const hubsRouter = require('./hubs/hubs-router.js');
+
 
 const server = express();
 
-server.use(express.json());
+function dateLogger(req, res, next) {//three amigos
+	console.log(new Date().toISOString());
+	console.log(`${req.baseURL}`)
+	console.log(`{req.method}`);
+	next();
+}
+//global middleware
+server.use(helmet());//third party
+server.use(express.json());//built-in
 
+//middlware only used in certain cases, restricting scope
 server.use('/api/hubs', hubsRouter);
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+server.get('/', (req, res) => {//req, res the homies
+	const nameInsert = (req.name) ? ` ${req.name}` : '';
 
-  res.send(`
+	res.send(`
     <h2>Lambda Hubs API</h2>
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
